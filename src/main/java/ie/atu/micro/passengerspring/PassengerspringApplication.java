32003da477f438080644314414d9c35c8f4ddf53
darragh <g00390383@atu.ie>
2022-12-06 17:36:48 +0000

    created spring project template for passenger

---
+++ src/main/java/ie/atu/micro/passengerspring/PassengerspringApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.micro.passengerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassengerspringApplication.class, args);
+	}
+
+}
