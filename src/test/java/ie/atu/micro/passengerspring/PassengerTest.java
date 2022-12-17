892f638272db133dcaf7bdf48efea25eb8a2fd95
darragh <g00390383@atu.ie>
2022-12-17 17:43:17 +0000

    Added postgres dependencies

--- src/test/java/ie/atu/micro/passengerspring/PassengerTest.java
+++ src/test/java/ie/atu/micro/passengerspring/PassengerTest.java
@@ -49,6 +49,35 @@ class PassengerTest {
         assertEquals(36, myPassenger.getAge());
     }
 
+    @Test
+    void testTitleFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {new Passenger("Mister", "John Doe", 1235473822, 1222222223, 19);});
+        assertEquals("This is not a valid title", exMessage.getMessage());
+    }
+
+    @Test
+    void testNameFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {new Passenger("Mr", "ls", 1235422173822L, 1222222223, 19);});
+        assertEquals("This is not a valid name", exMessage.getMessage());
+    }
+
+    @Test
+    void testIdFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {new Passenger("Mr", "John Doe", 123542, 1222222223, 19);});
+        assertEquals("This is not a valid ID", exMessage.getMessage());
+    }
+
+    @Test
+    void testPhoneFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {new Passenger("Mr", "John Doe", 1235423221213L, 122222, 19);});
+        assertEquals("This is not a valid phone number", exMessage.getMessage());
+    }
+
+    @Test
+    void testAgeFail(){
+        Exception exMessage = assertThrows(IllegalArgumentException.class, () -> {new Passenger("Mr", "John Doe", 1235423243243L, 1222222223, 13);});
+        assertEquals("You Are too Young", exMessage.getMessage());
+    }
 
 
     @AfterEach
